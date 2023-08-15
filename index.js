f63b0ac4e32e8129cae612b51ea8cfa366c42319
Tho Le Van <ThoLV2@fpt.com>
2023-08-15 09:18:32 +0700

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    err tslint
+    // err tslint
     res.end('Hello World! -> Capstone project.')
 })
 
